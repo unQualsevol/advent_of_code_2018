0ac8c344f7aeae958d45769ba0d3d6c53e018513
Aleix Giménez <kalagan@gmail.com>
2018-12-06 12:51:50 +0100

    Solution for fifth day

---
+++ src/com/github/unqualsevol/adventofcode2018/day05/ex1/FiveOne.kts
@@ -0,0 +1,50 @@
+import java.io.File
+
+
+val polymerAsString = File("input.txt").readText()
+
+
+val polymer = Polymer(polymerAsString)
+println(polymer.reduce())
+
+data class Polymer(val polymerAsString: String) {
+    private val polymerInChars = mutableListOf<Char>()
+
+    init {
+        polymerAsString.toCharArray().forEach { polymerInChars.add(it) }
+        //remove the EOF char
+        polymerInChars.removeAt(polymerInChars.size - 1)
+    }
+
+    fun reduce(): Int {
+        var i = 0;
+
+        while (i < polymerInChars.size - 1) {
+            if (react(i, i + 1)) {
+                if (i > 0) i--
+            } else {
+                i++
+            }
+        }
+        return polymerInChars.size;
+    }
+
+    private fun react(i: Int, j: Int): Boolean {
+        if (canReact(i, j)) {
+            polymerInChars.removeAt(j);
+            polymerInChars.removeAt(i);
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+    private fun canReact(i: Int, j: Int) = polymerInChars[i].equals(polymerInChars[j], true) &&
+            polymerInChars[i].isLowerCase().xor(polymerInChars[j].isLowerCase())
+
+
+    fun removeReactor(ch: Char) {
+        polymerInChars.removeAll { c -> c.equals(ch, true) }
+    }
+}
