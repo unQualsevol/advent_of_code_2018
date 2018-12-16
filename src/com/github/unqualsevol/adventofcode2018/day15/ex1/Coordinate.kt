34c7052a49461a28cb4315b6245f2025e5404116
Aleix Giménez <kalagan@gmail.com>
2018-12-16 03:15:58 +0100

    Solution for fifteenth day

---
+++ src/com/github/unqualsevol/adventofcode2018/day15/ex1/Coordinate.kt
@@ -0,0 +1,18 @@
+package com.github.unqualsevol.adventofcode2018.day15.ex1
+
+import kotlin.math.absoluteValue
+
+data class Coordinate(val row: Int, val column: Int) : Comparable<Coordinate> {
+    override fun compareTo(other: Coordinate): Int {
+        return compareValuesBy(this, other, { it.row }, { it.column })
+    }
+
+    fun manhattanDistance(coordinate: Coordinate): Int {
+        return (this.row - coordinate.row).absoluteValue + (this.column - coordinate.column).absoluteValue
+    }
+
+    fun adjacentCoordinates(): List<Coordinate> = listOf(Coordinate(row - 1, column),
+            Coordinate(row, column - 1),
+            Coordinate(row, column + 1),
+            Coordinate(row + 1, column))
+}
